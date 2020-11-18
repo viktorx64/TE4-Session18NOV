7a3050d8b9747e73d74586d00291f06cf22303ad
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-18 14:40:13 +0100

    tog bort onödig function

--- routes/notes.js
+++ routes/notes.js
@@ -32,7 +32,6 @@ router.post('/', function(req, res, next) {
     //fundera på JavaScript Object vs JSON som text, dvs vi har en Array med data
     //ta antalet poster och öka med 1
     //detta tal blir mitt nya id
-    var notesdata; 
         fs.readFile(dataPath, (err,data) =>{
             if(err) {
                 throw err;
@@ -46,35 +45,11 @@ router.post('/', function(req, res, next) {
                   console.log(err); 
               }); 
         });
-    
-    /*
-    fs.readFile(dataPath, (err,data) =>{
-        if(err) {
-            throw err;
-        }
-        //console.log(JSON.parse(data));
-        notesdata = JSON.parse(data);
-        //console.log(notesdata);
-        var newNotesId = Object.keys(notesdata).length + 1;
-        notesdata[newNotesId] = req.body.data;
-        //console.log(notesdata);
-        //fs.writeFile(dataPath, JSON.stringify(notesdata));
-    });
-    */
-
     //vi FÖRVÄNTAR oss att nu är notesdata populerat med data
     //MEN så är inte fallet!!
     //funktionen ovan fortsätter jobba och vi hamnar hör direkt
     //INNAN något värde har populerats till notesdata
     //console.log(notesdata); //varför funkar inte detta????
-
-    //console.log(notesdata[0].title);
-
-   //var newNotesId = Object.keys(notesdata).length + 1;
-    //notesdata[newNotesId] = req.body.data;
-
-    //fs.writeFile(dataPath, JSON.stringify(notesdata));
-
     res.status(200).send("new user added successfully");
     
     
