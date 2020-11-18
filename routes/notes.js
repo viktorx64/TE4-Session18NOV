d1704b8e18b88f1e0d0425a5419a9f96583dcda2
herkommer <m@herkommer.co.uk>
2020-11-18 12:01:27 +0100

    added non functional code for exercise

--- routes/notes.js
+++ routes/notes.js
@@ -17,13 +17,42 @@ router.get('/', function(req, res, next) {
 
 //POST a new Note
 router.post('/', function(req, res, next) {
-    fs.appendFile(dataPath, req.body.data, (err) =>{
+
+    //jag vill läsa in hela "filen" och ta reda på hur många poster som finns
+    //fundera på JavaScript Object vs JSON som text, dvs vi har en Array med data
+    //ta antalet poster och öka med 1
+    //detta tal blir mitt nya id
+
+    var notesdata; //= JSON.parse(fs.readFile(dataPath));
+
+    fs.readFile(dataPath, (err,data) =>{
         if(err) {
             throw err;
         }
-
-        res.send("new note added");
+        //console.log(JSON.parse(data));
+        notesdata = JSON.parse(data);
+        //console.log(notesdata);
+        var newNotesId = Object.keys(notesdata).length + 1;
+        notesdata[newNotesId] = req.body.data;
+        console.log(notesdata);
+        //fs.writeFile(dataPath, JSON.stringify(notesdata));
     });
+
+    //vi FÖRVÄNTAR oss att nu är notesdata populerat med data
+    //MEN så är inte fallet!!
+    //funktionen ovan fortsätter jobba och vi hamnar hör direkt
+    //INNAN något värde har populerats till notesdata
+    console.log(notesdata); //varför funkar inte detta????
+
+    //console.log(notesdata[0].title);
+
+   //var newNotesId = Object.keys(notesdata).length + 1;
+    //notesdata[newNotesId] = req.body.data;
+
+    //fs.writeFile(dataPath, JSON.stringify(notesdata));
+
+    res.status(200).send("new user added successfully");
+    
     
   });
 
