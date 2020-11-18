96b078243308777c2abecd0abe1bc721f158b221
herkommer <m@herkommer.co.uk>
2020-11-18 11:00:12 +0100

    added route for notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs'); //filesystem
+
+const dataPath = "./data/notes.json";
 
 /* GET all Notes. */
 router.get('/', function(req, res, next) {
-  res.send('notes coming soon');
+  fs.readFile(dataPath, (err,data) =>{
+      if(err) {
+          throw err;
+      }
+
+      res.send(JSON.parse(data));
+  });
 });
+
+
+router.post('/', function(req, res, next) {
+    res.send(req.body.data);
+  });
 
 module.exports = router;
